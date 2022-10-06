b1c5b4b9b5784de09c883e52c7aeee94a290dba2
youngju20 <young.ju2009@gmail.com>
2022-10-06 18:39:58 +0900

    correct setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
 	name='Mulguisin',
-	version='0.2',
+	version='0.4',
 	license='BSD',
 	description='Mulguisin: Cluster/group finding algorithm',
 	author='Young Ju, Inkyu Park, Sungwook E. Hong, Cristiano G. Sabiu',
 	author_email='youngju@uos.ac.kr',
-	packages=find_packages('Mulguisin',exclude=['test']),
-	package_dir={'': 'Mulguisin'},
+	packages=find_packages(),
+	#package_dir={'': 'Mulguisin'},
 	url='https://github.com/youngju20/Mulguisin',
 	install_requires=[
 		'scipy','numpy','h5py','numba',
